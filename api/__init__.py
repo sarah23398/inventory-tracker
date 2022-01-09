bb2b373b28c01b1adf8e2645113880e816148009
sarah23398 <sarah23398@yahoo.ca>
2022-01-09 02:47:10 -0500

    Adding SQLite db

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,11 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{os.getcwd()}/inventory.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
 
 @app.route('/')
