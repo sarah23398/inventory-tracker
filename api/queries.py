79e1ebe99f51db7e08f72b94df13c6ff30fcc354
sarah23398 <sarah23398@yahoo.ca>
2022-01-10 22:05:58 -0500

    Fetching a single item

--- api/queries.py
+++ api/queries.py
@@ -1,15 +1,27 @@
 from .models import Product, Warehouse, Shipment, Collection
 import sys
+from ariadne import convert_kwargs_to_snake_case
 
 def resolve_products(obj, info):
     products = [product.to_dict() for product in Product.query.all()]
     payload = products
     return payload
 
+@convert_kwargs_to_snake_case
+def resolve_product(obj, info, sku):
+    product = Product.query.get(sku)
+    payload = product.to_dict()
+    return payload
 
 def resolve_warehouses(obj, info):
     warehouses = [warehouse.to_dict() for warehouse in Warehouse.query.all()]
     payload = warehouses
+    return payload
+
+@convert_kwargs_to_snake_case
+def resolve_warehouse(obj, info, id):
+    warehouse = Warehouse.query.get(id)
+    payload = warehouse.to_dict()
     return payload
 
 def resolve_shipments(obj, info):
@@ -17,7 +29,19 @@ def resolve_shipments(obj, info):
     payload = shipments
     return payload
 
+@convert_kwargs_to_snake_case
+def resolve_shipment(obj, info, id):
+    shipment = Shipment.query.get(id)
+    payload = shipment.to_dict()
+    return payload
+
 def resolve_collections(obj, info):
     collections = [collection.to_dict() for collection in Collection.query.all()]
     payload = collections
+    return payload
+
+@convert_kwargs_to_snake_case
+def resolve_collection(obj, info, id):
+    collection = Collection.query.get(id)
+    payload = collection.to_dict()
     return payload
